392eb2db7b0f1e4db80a3a78afd627485b5e1581
LIRA-MIGUEL <122810468+LIRA-MIGUEL@users.noreply.github.com>
2023-01-24 19:59:27 +0000

    se creo el programa2.py, concatena o une variables de cualquier tipo de datos

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 |--|--|--|
 |1.|programa0.py|Hola mundo en python|
 |2.|programa1.py|concatenacion de cadenas|
+|3.|programa2.py|concatenacion usando format|
---
+++ semana2/programa2.py
@@ -0,0 +1,10 @@
+"""
+programa2.py 
+Nombre:Miguel Angel Lira Tellez
+Fecha:24/01/2023
+Descripcion:
+"""
+variable1= "Hola" #variable tipo
+variable2= 619 #variable tipo
+variable3="ya le se al python"#variable tipo
+print("{} {} {}".format(variable1,variable2,variable3))
