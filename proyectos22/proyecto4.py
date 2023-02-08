eb93864f4e6e20063880760501784e253510e63b
LIRA-MIGUEL <122810468+LIRA-MIGUEL@users.noreply.github.com>
2023-02-08 04:42:36 +0000

    se creo proyecto4.py del reto de los 22 dias programando

---
+++ proyectos22/proyecto4.py
@@ -0,0 +1,14 @@
+"""
+    programa1
+ nombre:Miguel Angel L. T.
+ fecha:07/02/2023
+ descripcion:
+ """
+numero1=int(input("numero1: "))
+numero2=int(input("numero2: "))
+if numero1<=numero2:
+    print(None)
+elif numero1!=numero2:
+    print(numero1)
+elif numero2!=numero1:
+    print(numero2)
