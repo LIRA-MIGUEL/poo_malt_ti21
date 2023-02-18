c313b0e8cb638c0a392913f7d69a24078810698c
LIRA-MIGUEL <122810468+LIRA-MIGUEL@users.noreply.github.com>
2023-02-18 05:27:13 +0000

    se actualizo el programa10.py

--- semana4/programa10.py
+++ semana4/programa10.py
@@ -4,15 +4,15 @@ Nombre:Miguel Angel Lira Tellez
 Fecha:09/02/2023
 Descripcion: uso del comando def y return
 """
-def mayor(numero1,numero2): # se asignan los numeroas
+def mayor(numero1,numero2): # se asignan los numeros, y se define una funcion 
     result=None# se asigna una funcion 
     if numero1>numero2: # con el if se agrega una condicion 
         result=numero1
     elif numero2>numero1: # se agrega otra solucion
         result=numero2
-    return result
+    return result # la funcion se detiene y devuelve el valor return 
 print(mayor(2,1)) #se hacen las pruebas
-print(mayor(1,2))
+print(mayor(1,2)) # si todas las pruebas se cumplen entoces estan bien 
 print(mayor(2,2))
 print(mayor(2,-1))
 print(mayor(-1,2))
