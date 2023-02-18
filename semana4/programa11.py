3db185d762c4ac8574a2974cf98b227405b3c575
LIRA-MIGUEL <122810468+LIRA-MIGUEL@users.noreply.github.com>
2023-02-18 05:33:03 +0000

    se actualizo el programa11.py

--- semana4/programa11.py
+++ semana4/programa11.py
@@ -4,13 +4,14 @@ Nombre:Miguel Angel Lira Tellez
 Fecha:7/0/2023
 Descripcion: Formas de resolver un problema 
 """
-def mayor(numero1:int,numero2:int)
+def mayor(numero1:int,numero2:int): # se asignan los numeros, y se define una funcion 
      result=None
-     if numero1>numero2:
+     if numero1>numero2:  #con el if se agrega una condicion 
         result=numero1
-     elif numero2>numero1:
+     elif numero2>numero1: #se agrega otra solucion
          result=numero2
-     return result
+     return result # la funcion se detiene y devuelve el valor return 
+    
 print(mayor(2,1))
 print(mayor(1,2))
 print(mayor(2,2))
