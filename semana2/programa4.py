65b2a35e86212a7a75852553695783389a717b80
LIRA-MIGUEL <122810468+LIRA-MIGUEL@users.noreply.github.com>
2023-02-19 22:33:21 -0600

    Update programa4.py
    se le agregaron comentarios al programa4.py

--- semana2/programa4.py
+++ semana2/programa4.py
@@ -2,14 +2,14 @@
 programa4.py 
 Nombre:Miguel Angel Lira Tellez
 Fecha:26/01/2023
-Descripcion:
+Descripcion: se uso el comando format para hacer una cadena de texto o datos 
 """
-numero1=10
-numero2=5
+numero1=10# se asignan variables 
+numero2=5#codigo sin errores 
 print("{}+{}={}".format(numero1,numero2,numero1+numero2))#solo imprime la suma 
-print("{}+{}={}".format(numero1,numero2,numero1+numero2)) # imprime la suma de manera correcta
+print("{}+{}={}".format(numero1,numero2,numero1+numero2)) # imprime la suma de manera correcta,enj una sola linea, formando una cadena "10+5=15"
 
-
+#codigo con algunos cambios de sintaxis, o cambiando el nombre de la avriable 
 print("{n1}+{n2}={suma}".format(n1=numero1,n2=numero2,suma=numero1+numero2))
 print("{n2}+{n2 {n2}".format(n1=numero1,n2=numero2,suma=numero1+numero2))#canbiando las variables usando el nombre, se cambio el n1,n2, suma por el n2
 print("{n1}+{n2}={suma}".format(n1=numero1,n2=numero2,suma=numero1+numero2)) #key error es cuando no encuentra la varible o no esta signada 
