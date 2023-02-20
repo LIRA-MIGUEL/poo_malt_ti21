d1cfa996b14ca09a7626778f8bc5feecc2c335f2
LIRA-MIGUEL <122810468+LIRA-MIGUEL@users.noreply.github.com>
2023-02-19 22:26:03 -0600

    Update programa11.py
    se le agrego un comentario al programa11.py

--- semana4/programa11.py
+++ semana4/programa11.py
@@ -13,7 +13,7 @@ def mayor(numero1:int,numero2:int): # se asignan los numeros, y se define una fu
      return result # la funcion se detiene y devuelve el valor return 
     
 print(mayor(2,1))
-print(mayor(1,2))
+print(mayor(1,2)) # se hacen las pruebas, si de cumplen todas significa que el rpoblema esta bien 
 print(mayor(2,2))
 print(mayor(2,-1))
 print(mayor(-1,2))
